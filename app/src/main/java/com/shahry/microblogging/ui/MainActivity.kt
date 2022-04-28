d9ba27058fc0be4656a204e3a415394ba670da2a
Ahmed <ahmed.nader1994@gmail.com>
2022-04-28 21:57:47 +0200

    enable viewbinding

---
+++ app/src/main/java/com/shahry/microblogging/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.shahry.microblogging.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shahry.microblogging.R
+import com.shahry.microblogging.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
